4989f8b3b4fcbbcc407bf4ccc830db7309745625
Rahul-9299 <bamrahul92@gmail.com>
2025-07-11 23:48:22 +0545

    Add files via upload

---
+++ built-in method/tuple.py
@@ -0,0 +1,10 @@
+tuple1=(1,2,2,3,3,3,3,4,4,4,5,5,5,5)
+
+#method: count
+count_4 = tuple1.count(4)
+print("Count of 4 in tuple1:", count_4)
+
+#method: index
+index_3 = tuple1.index(3)   
+print("Index of first occurrence of 3 in tuple1:", index_3)
+
