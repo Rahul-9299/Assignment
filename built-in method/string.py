e804bc8c6695d805c7d8225cce344cf7f99ef315
Rahul-9299 <bamrahul92@gmail.com>
2025-07-11 23:47:47 +0545

    Add files via upload

---
+++ built-in method/string.py
@@ -0,0 +1,23 @@
+s="My name is rahul bam." \
+"I am from surkhet."
+
+
+#method: upper
+s_upper = s.upper()
+print("Uppercase string:", s_upper)
+
+#method: lower
+s_lower = s.lower()
+print("Lowercase string:", s_lower)
+
+#method:find
+s_find = s.find("rahul")
+print("Index of 'rahul' in string:", s_find)
+
+#method: strip
+s_strip = s.strip()
+print("String after stripping whitespace:", s_strip)
+
+#methodr: replace
+s_replace = s.replace("rahul", "Rohit")
+print("String after replacing 'rahul' with 'Rohit':", s_replace)
