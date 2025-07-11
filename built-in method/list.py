20560ca1769f5966696f06285f9cb91e419c89c1
Rahul-9299 <bamrahul92@gmail.com>
2025-07-11 23:46:45 +0545

    Add files via upload

---
+++ built-in method/list.py
@@ -0,0 +1,21 @@
+list1=[1,2,3,4,5,6,7]
+
+#method: append
+list1.append(8)
+print("After appending 8 to list1:", list1)
+
+#method: remove
+list1.remove(2)
+print("After removing 2 from list1:", list1)
+
+#method: sort
+list1.sort()    
+print("Sorted list1:", list1)
+
+#method: copy
+list_copy = list1.copy()
+print("Copy of list1:", list_copy)
+
+#method: clear
+list1.clear()
+print("After clearing list1:", list1)
