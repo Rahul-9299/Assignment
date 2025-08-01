f53a44eb149084dce7ba50df4f30708ad11618e4
Rahul-9299 <bamrahul92@gmail.com>
2025-08-01 12:30:23 +0545

    Create file_handling.py

---
+++ assignment3/file_handling.py
@@ -0,0 +1,15 @@
+with open("grocery.txt","w") as f:
+    f.write("ID\t\tNAME\t\tPRICE\t\t\tCATEGORY\n")
+    records=[
+        {"name":"rice","price":120,"category":"grocery"},
+        {"name":"wheat","price":220,"category":"grocery"},
+        {"name":"sugar","price":320,"category":"grocery"},
+        {"name":"cereal","price":420,"category":"grocery"},
+    ]
+    for i in range(len(records)):
+        data=records[i]
+        f.write(f"{i+1}\t\t{data["name"]}\t\t{data["price"]}\t\t\t{data["category"]}\n")
+
+with open("grocery.txt","r") as f:
+    content= f.read()
+    print(content)
